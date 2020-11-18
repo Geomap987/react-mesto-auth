3b277490399fda243e36908d27fb8739a85a4b83
Geomap987 <buziuka@yandex.ru>
2020-11-18 19:45:38 +0300

    Updates

---
+++ precache-manifest.fefbc1125a394e67558198645992f1d9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da6bb04e92719b290a24c1dbd75ec3f",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "07171ef4d0d5ba93d786",
+    "url": "/mesto-react/static/css/main.5a695d70.chunk.css"
+  },
+  {
+    "revision": "89378c339242bb97ea3a",
+    "url": "/mesto-react/static/js/2.92b56ccf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mesto-react/static/js/2.92b56ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07171ef4d0d5ba93d786",
+    "url": "/mesto-react/static/js/main.7e62b4de.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "2a9832ce927763768e66da8bd5eb7697",
+    "url": "/mesto-react/static/media/AddButton.2a9832ce.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/mesto-react/static/media/Blackcheck.e38b395c.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/mesto-react/static/media/CloseIcon.c2d0901f.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "/mesto-react/static/media/EditButton.c8252db2.svg"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/mesto-react/static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/mesto-react/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/mesto-react/static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/mesto-react/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/mesto-react/static/media/Redcross.4072126c.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/mesto-react/static/media/delete.efa6fb04.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/mesto-react/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/mesto-react/static/media/likeactive.9fdebb6b.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/mesto-react/static/media/logo.855a8c98.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/vector.d476a8b8.svg"
+  }
+]);
